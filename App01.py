8f5ea6cec1a084b164bfb6023366d91be4cb45e3
Dcortesz705 <dcortesz705@gmail.com>
2024-08-15 10:13:21 -0500

    Update App01.py

--- App01.py
+++ App01.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image 
 
-st.title("Titulo")
-st.header("Subtitulo")
+st.title("Llama entrevistada")
+st.header("No es una llama, pero suena mas divertido que un venado")
 st.write("texto texto, mucho texto")
 image = Image.open("LlamaHabla.png")
 
-st.image(image, caption="Interfaces multimodales")
+st.image(image, caption="muy seria, no sabemos de que era la entrevista")
 
