e2383d9861ee0a4e4435c4b32c519fd935595f2a
Dcortesz705 <dcortesz705@gmail.com>
2024-08-15 10:25:44 -0500

    Update App01.py

--- App01.py
+++ App01.py
@@ -10,3 +10,28 @@ st.image(image, caption="muy seria, no sabemos de que era la entrevista")
 
 texto = st.text_input("escribo aca algo diferente", "Demasiado diferente")
 st.write("el texto escrito es: ", texto)
+
+with colum1:
+  st.subheader ("esta es la primera columna")
+  st.write("texto texto texto y mas texto... mucho texto")
+  resp = st.checkbox("definitivamente es mucho texto")
+  if resp:
+    st.write("Correcto")
+
+with colum2:
+  st.subheader ("Esta es la segunda columna")
+  modo = st.radio("Cual es el animal de la imagen", ("Venado" , "Llama", "Ciervo"))
+  if modo == "Venado":
+    st.write("seguro?")
+  if modo == "Venado":
+    st.write("Tal vez si, tal vez no")
+  if modo == "Ciervo":
+    st.write("si tu lo dices...")
+
+
+
+
+
+
+    
+
