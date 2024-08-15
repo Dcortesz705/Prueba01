b772f9e5e7a1286bccc03e883d0ddcc65ea4581d
Dcortesz705 <dcortesz705@gmail.com>
2024-08-15 10:10:52 -0500

    Update App01.py

--- App01.py
+++ App01.py
@@ -4,4 +4,7 @@ from PIL import Image
 st.title("Titulo")
 st.header("Subtitulo")
 st.write("texto texto, mucho texto")
-image = Image.open(Imagen.jfif)
+image = Image.open(LlamaHabla.png)
+
+st.image(image, caption="Interfaces multimodales")
+
